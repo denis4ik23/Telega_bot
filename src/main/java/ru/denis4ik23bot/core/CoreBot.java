5880b7aef46c3a8e05de65ced948d68a0c601c4b
denis4ik23 <denis4ik2387@gmail.com>
2022-01-14 00:25:22 +0300

    2 core

--- src/main/java/ru/denis4ik23bot/core/CoreBot.java
+++ src/main/java/ru/denis4ik23bot/core/CoreBot.java
@@ -4,18 +4,19 @@ import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.objects.Update;
 //создаем ядро бота
 public class CoreBot extends TelegramLongPollingBot {
+    //метод возвращает username который указали при регистрации
     @Override
     public String getBotUsername() {
-        return null;
-    }
-
+        return "denis4ik23_bot";
+    }//username
+    //метод возвращает токен который дали при регистрации
     @Override
     public String getBotToken() {
-        return null;
+        return "5038609304:AAE13S9XauNgaEWY9Am6DU2rgk00jSjrJ1A";//токен
     }
     //обрабатывает сообщения от пользователя и отвечать
     @Override
     public void onUpdateReceived(Update update) {
-
+        System.out.println("test");
     }
 }
